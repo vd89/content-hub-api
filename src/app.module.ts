23a432f1c9e452785cdd5a6f5760c181504d4c85
Dixit Vora <dixitvora@gmail.com>
2025-11-28 00:51:59 +0530

    setup: added the auth module using generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { AppService } from './app.service';
       isGlobal: true, // Makes the config module global
       envFilePath: '.env', // Path to your .env file
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
